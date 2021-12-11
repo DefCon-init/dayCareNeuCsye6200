8a39a70c1e046dfa82200f29bd14ed3aa169cf1e
Shubhang Shah <shubhang34@gmail.com>
2021-12-10 21:05:07 -0500

    Intialize Daycare Class

---
+++ src/edu/neu/csye/daycare/Daycare.java
@@ -0,0 +1,8 @@
+package edu.neu.csye.daycare;
+
+public class Daycare {
+    public static void demo() {
+        System.out.println("Daycare run");
+    }
+
+}
--- src/edu/neu/csye/daycare/Driver.java
+++ src/edu/neu/csye/daycare/Driver.java
@@ -3,7 +3,7 @@ package edu.neu.csye.daycare;
 public class Driver {
 	
 	public static void main(String[] args) {
-//		Daycare.demo();
+		Daycare.demo();
 	}
 
 }
