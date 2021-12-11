99580ad11915b233883d7ea0ae0a4a4d76d2af7f
RishabhModi2899 <56045320+RishabhModi2899@users.noreply.github.com>
2021-12-10 21:30:50 -0500

    feat: Add Person Class and Person factory

---
+++ src/edu/neu/csye/daycare/Person.java
@@ -0,0 +1,20 @@
+package edu.neu.csye.daycare;
+
+public abstract class Person {
+/*
+    Abstract Class Person ( Blueprint for Teacher and Student )
+ */
+
+    public abstract int getAge();
+
+    public abstract void setAge(int age);
+
+    public abstract String getFirstName();
+
+    public abstract void setFirstName(String firstName);
+
+    public abstract String getLastName();
+
+    public abstract void setLastName(String lastName);
+
+}
